03c522d125367b9098b59159245628b1f077e021
Todd Mathison <mathist@yahoo.com>
2024-11-07 10:49:12 -0800

    Initial commit

---
+++ FetchRecipesTests/FetchRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesTests.swift
+//  FetchRecipesTests
+//
+//  Created by Todd Mathison on 11/7/24.
+//
+
+import Testing
+@testable import FetchRecipes
+
+struct FetchRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
